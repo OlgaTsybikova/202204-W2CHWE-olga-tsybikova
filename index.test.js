bcaae9d60d086bb8b237f19ec403b98b32479dcc
Olga Tsybikova <tsy2701@gmail.com>
2022-04-16 17:24:29 +0200

    started creating basic set up of the game

---
+++ index.test.js
@@ -0,0 +1,8 @@
+function liveNeighbors () 
+describe(Given a liveNeighbors function, => () {
+  describe(When it receives a board 3x3 with the postion of the cell in the center),=> (){
+    const x = 0; 
+    const y = 0;
+    const expectedResult = 
+  }
+})
