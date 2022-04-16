3393c91c8375e75fabf53cfb112af44b20ec5e2d
Olga Tsybikova <tsy2701@gmail.com>
2022-04-16 13:41:36 +0200

    Modified error por audit

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const board = [
   [0, 0, 0],
   [0, 0, 0],
 ];
-
 class GridOfCells {
   status = 0;
   x;
@@ -15,4 +14,22 @@ class GridOfCells {
   }
 }
 
-const game = new GridOfCells(0, 1, 0);
+const cell = new GridOfCells(0, 0);
+const neighbour = 0;
+
+if (neighbour >= 3) {
+  cell.status = 0;
+} else if (neighbour < 3) {
+  cell.status = 1;
+} else if (cell.status === 1) {
+  cell.status = 0;
+} else {
+  cell.status = 1;
+}
+
+board();
+
+// if a living cell has more than three neighbors, it dies
+// if a living cell has less than three neighbors, it dies
+// if a dead cell has exactly three neighbors — it comes to life
+// in other cases, the cell remains in its state
