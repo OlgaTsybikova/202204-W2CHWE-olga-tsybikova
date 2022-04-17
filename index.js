c27e4c8e7d24306214e488537c0df4836e90dbd3
Olga Tsybikova <tsy2701@gmail.com>
2022-04-17 17:16:47 +0200

    Worked on the function to check neighbors, still not working

--- index.js
+++ index.js
@@ -1,56 +1,70 @@
 const canvas = document.getElementById("canvasId");
-const context = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 let cell = [];
 
-function drawCell() {
-  context.clearRect(0, 0, 300, 300);
+function drawField() {
+  ctx.clearRect(0, 0, 300, 300);
   for (let i = 0; i < 30; i++) {
     for (let j = 0; j < 30; j++) {
       if (cell[i][j] === 1) {
-        context.fillRect(j * 10, i * 10, 10, 10);
+        ctx.fillRect(j * 10, i * 10, 10, 10);
       }
     }
   }
 }
-canvas.onclick = function coordinatesCreator(event) {
+
+canvas.onclick = function cellCoordinates(event) {
   let x = event.offsetX;
   let y = event.offsetY;
-  x = Math.floor(x / 10);
+  x = Math.floor(x / 10); // 300 /10 = 30
   y = Math.floor(y / 10);
   cell[y][x] = 1;
-  drawCell();
+
+  drawField();
 };
 
-function gameBoard() {
-  const row = 30;
-  const column = 30;
-  for (let i = 0; i < row; i++) {
+function goLife() {
+  const n = 30;
+  const m = 30;
+  for (let i = 0; i < m; i++) {
     cell[i] = [];
-    for (let j = 0; j < column; j++) {
+    for (let j = 0; j < n; j++) {
       cell[i][j] = 0;
     }
   }
 }
-gameBoard();
+goLife();
+
+function goOutIfMeetLimit(i) {
+  if (i === 0) {
+    return 30;
+  }
+  return i;
+}
 
 function startLife() {
+  // моделирование жизни
   const cellLife = [];
   for (let i = 0; i < 30; i++) {
     cellLife[i] = [];
     for (let j = 0; j < 30; j++) {
-      let neighbours = 0;
-      if (cellLife[i][j] === undefined) {
-        neighbours++;
-      }
-      if (neighbours === 2 || neighbours === 3) {
+      let neighbors = 0;
+      if (cell[goOutIfMeetLimit(i) - 1][j] === 1) {
+        neighbors++;
+      } else if (cell[i][goOutIfMeetLimit(j) + 1] === 1) {
+        neighbors++;
+      } else if (cell[i][goOutIfMeetLimit(j) - 1] === 1) {
+        neighbors++; // left
+      } else if (cell[goOutIfMeetLimit(i) - 1][goOutIfMeetLimit(j) - 1] === 1) {
+        neighbors++;
+      } else if (neighbors === 2 || neighbors === 3) {
         cellLife[i][j] = 1;
-      } else {
-        cellLife[i][j] = 0;
       }
+      cellLife[i][j] = 0;
     }
   }
   cell = cellLife;
+  drawField();
 }
-drawCell();
 
-document.getElementById("start").onclick = startLife();
+document.getElementById("start").onclick = startLife;
